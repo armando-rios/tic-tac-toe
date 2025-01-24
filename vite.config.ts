1a7b32c98cfebf882ce5bbaa11edef1d4d433f72
Armando Rios <armandorios.dev@gmail.com>
2025-01-24 06:32:15 -0800

    chore: add base repo on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/tic-tac-toe/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     tailwindcss(),
   ],
